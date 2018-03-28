a13019d3212d858b604da5cabfb302b4e2109322
sophieChenyx <sophiechenyong@gmail.com>
2018-03-28 14:36:14 +0800

    rm all

--- src/main.js
+++
@@ -1,7 +0,0 @@
-
-require('./assets/css/style.css');
-
-var img1 = document.createElement('img');
-img1.src = require('./assets/img/chenyx.png')
-document.body.appendChild(img1);
-document.write('hello world');
